af6318004c807ecf8d6698db816a204d8d912eaa
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-01 18:26:36 -0300

    Exercício 1 - Crie as rotas necessárias para a aplicação

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Route } from 'react-router-dom';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <h1>LOGIN</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
