4432f29fb9b2755f8dd64809771008dc7acd4130
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-05 21:07:26 -0300

    Exercício 5 - Crie o formulário para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,55 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const caractNum = 2;
+    if (event.value.length >= caractNum) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  }
+
+  handleOnChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.handleChange(target);
+  }
+
   render() {
+    const { disabled, nome } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <label htmlFor="search-artist-input">
+          <input
+            type="text"
+            name=""
+            data-testid="search-artist-input"
+            onChange={ this.handleOnChange }
+            value={ nome }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
