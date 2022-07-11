0919c4dcbd65105c45f8146da7c0c193a74c16d0
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-11 14:38:12 -0300

    Exercício 8 - Crie o mecanismo para adicionar músicas na lista de músicas favoritas

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -1,20 +1,60 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { addSong } from '../services/favoriteSongsAPI';
+// import { musica } from "../services/musicsAPI";
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  state = {
+    checkin: false,
+    onLoading: false,
+  }
+
+  checkin = async () => {
+    this.setState({ onLoading: true });
+    const { music } = this.props;
+    await addSong(music);
+    console.log('ok');
+    this.setState({ checkin: true, onLoading: false });
+  }
+
   render() {
     const { music } = this.props;
-    const { trackName, previewUrl } = music;
+    const { trackName, previewUrl, trackId } = music;
+    const { checkin, onLoading } = this.state;
     return (
       <div>
-        <h5>{trackName}</h5>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
+
+        {onLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <h6>{trackName}</h6>
+            <form>
+
+              <label htmlFor={ `checkbox-music-${trackId}` }>
+                Favorita
+
+                <input
+                  type="checkbox"
+                  name=""
+                  data-testid={ `checkbox-music-${trackId}` }
+                  onChange={ this.checkin }
+                  checked={ checkin }
+                />
+
+              </label>
+            </form>
+
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+          </>
+        )}
       </div>
     );
   }
