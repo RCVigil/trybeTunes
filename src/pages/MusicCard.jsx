17a86d7bdcfc0cd5a5d3f65c9083105c8657ccec
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-11 21:06:10 -0300

    Exercicio 10 - Faça a requisição para recuperar as músicas favoritas e atualizar a lista após favoritar uma música

--- src/pages/MusicCard.jsx
+++ src/pages/MusicCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 // import { musica } from "../services/musicsAPI";
 import Loading from './Loading';
 
@@ -10,11 +10,20 @@ class MusicCard extends Component {
     onLoading: false,
   }
 
+  // Ao favoritar uma música, aguarde o retorno da função addSong (que já foi implementada no requisito 8) e utilize a função getFavoriteSongs da favoriteSongsAPI para recuperar a lista de músicas favoritas.
+
+  // favoriteMusic = async () => {
+  //   this.setState({ onLoading: true });
+  //   const { music } = this.props;
+  //   console.log();
+  //   this.setState({ onLoading: false });
+  // }
+
   checkin = async () => {
     this.setState({ onLoading: true });
     const { music } = this.props;
     await addSong(music);
-    console.log('ok');
+    await getFavoriteSongs(music);
     this.setState({ checkin: true, onLoading: false });
   }
 
@@ -37,10 +46,11 @@ class MusicCard extends Component {
 
                 <input
                   type="checkbox"
-                  name=""
+                  name="checkboxFavorites"
                   data-testid={ `checkbox-music-${trackId}` }
                   onChange={ this.checkin }
                   checked={ checkin }
+                  // value={ () => favoriteMusic }
                 />
 
               </label>
